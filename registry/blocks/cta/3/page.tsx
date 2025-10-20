9c3f5777f043ad91199864a263887d6d445696f5
Shaban <sshahaider@gmail.com>
2025-10-20 15:50:49 +0500

    + 5 cta blocks

---
+++ registry/blocks/cta/3/page.tsx
@@ -0,0 +1,9 @@
+import { CallToAction } from "./cta";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center p-4">
+      <CallToAction />
+    </div>
+  );
+}
