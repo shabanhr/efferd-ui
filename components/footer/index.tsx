6dc142c43735d1767a96a9dec84e7818d0f4b78e
shabanhr <sshahaider@gmail.com>
2025-11-04 23:02:55 +0500

    + add image in footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,16 +4,23 @@ import { MY_HANDLE, SITE_NAME } from "@/config/site";
 export function SiteFooter() {
   return (
     <footer className="border-t border-dashed bg-card dark:bg-card/50">
-      <div className="cpx container flex items-center justify-between py-6 text-xs md:text-sm">
+      <div className="cpx container flex items-center justify-between py-5 text-xs md:text-sm">
         <p className="flex items-center gap-1 text-muted-foreground">
           <span>Built by</span>
           <Link
             aria-label="x/twitter"
-            className="font-medium text-foreground/90 hover:text-foreground hover:underline"
+            className="flex items-center gap-1 text-foreground/80 hover:text-foreground hover:underline"
             href={`https://x.com/${MY_HANDLE}`}
             rel="noreferrer"
             target="_blank"
           >
+            <img
+              alt="shaban"
+              className="size-4 rounded-full"
+              height="auto"
+              src="/shaban.webp"
+              width="auto"
+            />
             Shaban
           </Link>
         </p>
